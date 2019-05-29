64449111c87a535f9add6b6eccfa4de9675533b1
Jake Miller <jake0miller@gmail.com>
2019-05-29 13:24:08 -0600

    Finish iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -17,4 +17,16 @@ class Market
   def vendors_that_sell(item)
     @vendors.find_all {|vendor| vendor.inventory.has_key? item}
   end
+
+  def sorted_item_list
+    @vendors.each_with_object([]) do |vendor,arr|
+      arr << vendor.inventory.keys
+    end.flatten.uniq.sort
+  end
+
+  def total_inventory
+    @vendors.each_with_object(Hash.new(0)) do |vendor,hash|
+      vendor.inventory.each { |item,num| hash[item] += num }
+    end
+  end
 end
